a3bccdc52fc288defbfed4844f5f0ccecf9d6b79
Gregory Banks <gbanks1438@gmail.com>
2021-11-04 09:48:53 -0400

    commit tree f11a367fd84c54eda724944dee32e5c66d344cb6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
 
+function superbowlWin(record) {
+    let win = record.find(record => record.result === "W");
+    if (win){
+        return win.year;
+    }      
+}
